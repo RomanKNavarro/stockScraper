70343b60feec9443bf40e84f5ab76a88e8b98b16
roman <ronnoverro@gmail.com>
2024-03-11 10:39:13 -0700

    more progress

--- stockScraper.py
+++ stockScraper.py
@@ -8,38 +8,64 @@
     - special "Graham Number"
 '''
 
-# from bs4 import BeautifulSoup 
 import bs4
 import requests
+import math
 
 # yahoo finance stock url: https://finance.yahoo.com/quote/ATVIX?.tsrc=fin-srch
 # https://finance.yahoo.com/quote/STOCKSYMBOLHERE?.tsrc=fin-srch
 
 while True:
-    print("INPUT TICKER SYMBOL")
+    print("\nINPUT TICKER SYMBOL")
     ticker = input().lower()
-    # url = 'https://finance.yahoo.com/quote/%s?.tsrc=fin-srch' % ticker
-    # url = 'https://finance.yahoo.com/quote/HD/'
-    # url = 'https://stockanalysis.com/stocks/hd/statistics/'
 
     url = 'https://stockanalysis.com/stocks/%s/statistics/' % ticker
 
-    print(url)
-
     res = requests.get(url)
-    print(res.status_code)  # 200 if successful
 
     if res.status_code == 200:
-        print('ACCESS SUCCESSFUL!: %s\n' % url)
+        print('(200) ACCESS SUCCESSFUL! (%s)\n' % url)
         mainSoup = bs4.BeautifulSoup(res.text, features="html.parser")  # html parser
 
         # 1. start off by getting Company name
+        name = mainSoup.find('h1')
+        print(name.get_text())
 
-        # results = mainSoup.find(id='atomic')
+        price = mainSoup.find('div', class_='text-4xl font-bold inline-block').get_text()
 
-        results = mainSoup.find('h1')
-        # name = results.find_all()
-        print(results.get_text())
+        stats = mainSoup.find_all('td', class_='px-[5px] py-1.5 text-right font-semibold xs:px-2.5 xs:py-2')
+
+        # PRICE
+        print('PRICE: %s' % price)
+
+        # EPS
+        eps = stats[56].get_text()
+        print('EPS: %s' % eps)
+
+        # PE
+        pe = stats[10].get_text()
+        print('P/E RATIO: %s' % pe)
+
+        # ROE
+        roe = stats[28].get_text()
+        print('ROE: %s' % roe)
+
+        # BVPS
+        bvps = stats[62].get_text()
+        print('BVPS: %s' % bvps)
+
+        # PBR
+        priceNum = float(str(price))
+        bvpsNum = float(str(bvps))
+        print(f'PBR: {(priceNum / bvpsNum):.2f}')
+
+        # GRAHAM NUMBER
+        # sqrt(22.5 * eps * bvps)
+
+        epsNum = float(str(eps[1: -1]))
+        grahamNum = math.sqrt(22.5 * epsNum * bvpsNum)
+        print(f'GRAHAM #: {grahamNum:.2f}')
+
     else:
         print('404: Access Denied!')
 
