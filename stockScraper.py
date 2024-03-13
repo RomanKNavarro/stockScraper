a2ad7d54da08525ebfbd1f20ae5b13de80050071
roman <ronnoverro@gmail.com>
2024-03-13 14:25:01 -0700

    red text added

--- stockScraper.py
+++ stockScraper.py
@@ -1,4 +1,4 @@
-''' stockScraper.py: given a ticker, analyzes the respective Stock's following info:
+''' stockScraper.py: given a ticker, analyze the respective Stock's following info:
     - Company Name
     - EPS
     - P/E Ratio
@@ -12,73 +12,80 @@ import bs4
 import requests
 import math
 
+from colorist import red, ColorRGB
+
 # yahoo finance stock url: https://finance.yahoo.com/quote/ATVIX?.tsrc=fin-srch
 # https://finance.yahoo.com/quote/STOCKSYMBOLHERE?.tsrc=fin-srch
 
+red = ColorRGB(200, 0, 0)
+
+print(f"kill the {red}vip{red.OFF}")
+
 while True:
-    print("\nINPUT TICKER SYMBOL")
+    print("\nINPUT TICKER SYMBOL (OR 'help' FOR HELP)")
     ticker = input().lower()
 
     url = 'https://stockanalysis.com/stocks/%s/statistics/' % ticker
 
     res = requests.get(url)
 
-    if res.status_code == 200:
-        print('(200) ACCESS SUCCESSFUL! (%s)\n' % url)
-        mainSoup = bs4.BeautifulSoup(res.text, features="html.parser")  # html parser
+    if ticker != 'help':
+        if res.status_code == 200:
+            print('(200) ACCESS SUCCESSFUL! (%s)\n' % url)
+            mainSoup = bs4.BeautifulSoup(res.text, features="html.parser")  # html parser
 
-        # 1. start off by getting Company name
-        name = mainSoup.find('h1')
-        print(name.get_text())
+            # 1. start off by getting Company name
+            name = mainSoup.find('h1')
+            print(name.get_text())
 
-        price = mainSoup.find('div', class_='text-4xl font-bold inline-block').get_text()
+            # PRICE
+            price = mainSoup.find('div', class_='text-4xl font-bold inline-block').get_text()
 
-        stats = mainSoup.find_all('td', class_='px-[5px] py-1.5 text-right font-semibold xs:px-2.5 xs:py-2')
+            # IT SOMETIMES ALTERNATES TO THIS:
+            # price = mainSoup.find('div', class_='text-4xl font-bold block sm:inline').get_text()
 
-        # PRICE
-        print('PRICE: %s' % price)
+            # for everything else
+            stats = mainSoup.find_all('td', class_='px-[5px] py-1.5 text-right font-semibold xs:px-2.5 xs:py-2')
 
-        # EPS
-        eps = stats[56].get_text()
-        print('EPS: %s' % eps)
+            # EPS
+            eps = stats[56].get_text()
 
-        # PE
-        pe = stats[10].get_text()
-        print('P/E RATIO: %s' % pe)
+            # PE
+            pe = stats[10].get_text()
 
-        # ROE
-        roe = stats[28].get_text()
-        print('ROE: %s' % roe)
+            # ROE
+            roe = stats[28].get_text()
 
-        # BVPS
-        bvps = stats[62].get_text()
-        print('BVPS: %s' % bvps)
+            # BVPS
+            bvps = stats[62].get_text() 
 
-        # PBR
-        priceNum = float(str(price))
-        bvpsNum = float(str(bvps))
-        print(f'PBR: {(priceNum / bvpsNum):.2f}')
+            # PBR
+            priceNum = float(str(price))
+            bvpsNum = float(str(bvps))
+            pbrNum = priceNum / bvpsNum
+            # print(f'PBR: {pbrNum:.2f}')
 
-        # GRAHAM NUMBER
-        # sqrt(22.5 * eps * bvps)
+            epsNum = float(str(eps[1: -1]))
+            grahamNum = str(round(math.sqrt(22.5 * epsNum * bvpsNum), 2))
+            #print(f'GRAHAM #: {grahamNum:.2f}')
 
-        epsNum = float(str(eps[1: -1]))
-        grahamNum = math.sqrt(22.5 * epsNum * bvpsNum)
-        print(f'GRAHAM #: {grahamNum:.2f}')
+            stats = {"PRICE": price, "EPS": eps, "PE": pe, "ROE": roe, "BVPS": bvps, "GRAHAM #": grahamNum}
 
+            for name, num in stats.items():
+                print(name.ljust(20) + num.rjust(20))
+
+        else:
+            print('404: Access Denied!')
     else:
-        print('404: Access Denied!')
-
-
-
-    #     # scraping logic:
-    #     mainSoup = bs4.BeautifulSoup(res.text, features="html.parser")  # html parser
-
-    #     # 1. start off by getting Company name
-    #     # nameElem = mainSoup.select('.D(ib) Mend(20px)')
-    #     results = mainSoup.find(id='atomic')
-    #     name = results.find_all('title')  # element type goes in here
-    #     print(name[0].text[0])
-
-
-    # res.raise_for_status()
+        print(f"""
+{red}PE:{red.OFF} below 10 is considered low, between 20 & 20 is moderate, 
+    and greater than 20 is expensive
+    
+{red}ROE:{red.OFF} the higher the ROE, the better a company is at converting it's
+    equity financing into profits
+    
+{red}BVPS:{red.OFF} If company's BVPS is higher than it's current stock price, then the
+    stock's considered undervaluied.
+    
+{red}PBR:{red.OFF} value of 1 means thst stock's trading in-line w/ BV. Below signals
+    a potentially undervalued stock. Above means it's trading @ a premium to the BV.""")
